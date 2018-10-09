1373fdace9e044340ba17fc2b3f8d63bbc7b4657
Brent <brent@recranet.com>
2018-10-09 08:15:53 +0200

    Rename hightlights to highlights

--- mod_recranet_highlights.php
+++ mod_recranet_highlights.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  mod_recranet
+ * @subpackage  mod_recranet_highlights
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -15,4 +15,4 @@ $html5Mode = (boolean) $params->get('html5Mode');
 
 $accommodations = file_get_contents('https://app.recranet.com/api/accommodations/?active=true&category=0&locale=' . $lang . '&organization=' . $org . '&public=true&tzoffset=120&featured=true');
 
-require JModuleHelper::getLayoutPath('mod_recranet_hightlights', 'default');
+require JModuleHelper::getLayoutPath('mod_recranet_highlights', 'default');
